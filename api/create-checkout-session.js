b8956348d46e972e95282b44d9a8ce95669f2802
Whiskey34 <black.delfino28@gmail.com>
2025-09-22 06:08:55 +0700

    fixing cors 3

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-app.post("/api/create-checkout-session", async (req, res) => {
+app.post("/", async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],

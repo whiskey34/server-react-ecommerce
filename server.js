ccebea96edd8001e274a1ad707c11f2f3e130204
Whiskey34 <black.delfino28@gmail.com>
2025-09-21 15:10:17 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "https://react-ecommerce-three-inky.vercel.app/" }));
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post("/api/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map((item) => ({
+        price_data: {
+          currency: "usd",
+          product_data: { name: item.name },
+          unit_amount: item.price * 100, // cents
+        },
+        quantity: item.quantity,
+      })),
+      success_url: "http://localhost:5173/success",
+      cancel_url: "http://localhost:5173/cancel",
+    });
+
+    res.json({ url: session.url });
+  } catch (err) {
+    console.error("Stripe error:", err);
+    res.status(500).json({ error: "Payment failed" });
+  }
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));

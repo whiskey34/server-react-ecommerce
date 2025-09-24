6097220df3be13973ff6cbcd3c801f7b545013a5
Whiskey34 <black.delfino28@gmail.com>
2025-09-24 08:33:47 +0700

    change cors allow origin url

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://react-ecommerce-three-inky.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://bitshop.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.sendStatus(200);
